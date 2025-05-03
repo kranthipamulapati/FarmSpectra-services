8f48ffb211e82c747c5d76652d29449778819429
Kranthi Pamulapati <pamulapati.kranthi@gmail.com>
2025-05-03 09:20:57 +0530

    metadata route removed

--- app.ts
+++ app.ts
@@ -10,14 +10,12 @@ import { calendarRouter } from "./src/routes/farms/calendar";
 import { dataRouter } from "./src/routes/farms/satellite/data";
 import { taskRouter } from "./src/routes/farms/satellite/task.ts";
 import { processRouter } from "./src/routes/farms/satellite/process";
-import { metadataRouter } from "./src/routes/farms/satellite/metadata";
 
 app.use(swagger());
 app.use(dataRouter);
 app.use(taskRouter);
 app.use(farmsRouter);
 app.use(processRouter);
-app.use(metadataRouter);
 app.use(calendarRouter);
 
 app.use(getFarmsSatelliteDataCron);
